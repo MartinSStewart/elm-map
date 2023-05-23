f6e9a9117af7688a5da31ac585385ff3cafdab6f
Martin Stewart <MartinSStewart@gmail.com>
2023-05-23 13:09:38 +0200

    Add comment about API key

--- example/src/Main.elm
+++ example/src/Main.elm
@@ -16,6 +16,8 @@ type Msg
     = MapMsg MapViewer.Msg
 
 
+{-| This should be safe to commit to the repo. This Mapbox API key is publically scoped which as I understand it means it's intended to be shared with the end user <https://docs.mapbox.com/help/troubleshooting/how-to-use-mapbox-securely/#access-tokens>
+-}
 mapboxApiKey =
     MapViewer.mapboxAccessToken "pk.eyJ1IjoiYXQxMjMxMjMxMjMiLCJhIjoiY2t0dWh2NW9rMWE0czMycWVleWJsMWNxcyJ9.rhtVgETrZT4_LboePjCCpA"
 
